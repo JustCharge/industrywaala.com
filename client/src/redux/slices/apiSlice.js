08bdcb12ca22762c6c60a8be952447d3ea1dad90
Just Charge Technologies <167403327+JustCharge@users.noreply.github.com>
2024-05-09 19:29:26 +0530

    Update apiSlice.js

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // const API_URL = import.meta.env.VITE_APP_BASE_URL + "/api";
-const API_URL = "https://industrywaala-com.vercel.app/";
+const API_URL = "industrwala-backend.netlify.app";
 
 const baseQuery = fetchBaseQuery({ baseUrl: API_URL });
 
